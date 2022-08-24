3707d0efbd1b8ed65c32d28f2dff5407c851f446
s-parshev <stoyan.parshev@novarto.com>
2022-08-24 15:46:02 +0300

    50 artists added

--- src/pages/napster/rank-list.js
+++ src/pages/napster/rank-list.js
@@ -1,6 +1,6 @@
 async function getData() {
   let obj = {};
-  let url = "https://api.napster.com/v2.2/tracks/top?limit=5&apikey=MzdhZDE3OTMtZDI0ZC00YmQ4LWI1ODEtOGNiOTdiYTQwMWM0";
+  let url = "https://api.napster.com/v2.2/tracks/top?limit=50&apikey=MzdhZDE3OTMtZDI0ZC00YmQ4LWI1ODEtOGNiOTdiYTQwMWM0";
   obj = await fetch(url).then((response) => response.json());
   return obj;
 }
