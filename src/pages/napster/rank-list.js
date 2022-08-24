ba3fed2e108c5e529cda3e3234582e4e2337c773
s-parshev <stoyan.parshev@novarto.com>
2022-08-24 14:26:25 +0300

    array of artists

--- src/pages/napster/rank-list.js
+++ src/pages/napster/rank-list.js
@@ -1,20 +1,22 @@
-// async function getData(){
-// const data = await fetch("https://api.napster.com/v2.2/artists/Art.28463069/albums?apikey=MzdhZDE3OTMtZDI0ZC00YmQ4LWI1ODEtOGNiOTdiYTQwMWM0")
-//   .then(res=>console.log(res))
-//   console.log(data)
-// }
-
 async function getData() {
   let obj = {};
   let url = "https://api.napster.com/v2.2/tracks/top?limit=5&apikey=MzdhZDE3OTMtZDI0ZC00YmQ4LWI1ODEtOGNiOTdiYTQwMWM0";
-  obj =await fetch(url).then(response => response.json())
-
-    return obj;
+  obj = await fetch(url).then((response) => response.json());
+  return obj;
 }
-
- async function f1() {
+async function f1() {
   let data = await getData();
-  console.log(data);
-}
+  const artist = [];
+  // let trackName  ===name in object
+  // let playMusic ===previewURL in Object
+  // selectedProperty = data.tracks.map(arr=>{
 
+  // })
+  data.tracks.forEach((element, index) => {
+    const { artistName, albumName, name, previewURL, ...rest } = element;
+    let artistObj = { artistName:`${artistName}`,albumName:`${albumName}`, name:`${name}`, previewURL:`${previewURL}` };
+    artist.push(artistObj)
+  });
+  console.log(artist)
+}
 f1();
