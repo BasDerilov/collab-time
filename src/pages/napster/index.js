09daea3a6400cd248f3075fb0404ab2a91cc4e1c
Vasil Derilov <vasil.derilov@gmail.com>
2022-08-24 18:07:54 +0300

    almost finished

--- src/pages/napster/index.js
+++ src/pages/napster/index.js
@@ -1,22 +1,52 @@
 import { GetMusic } from "./rank-list"
 
 const container = document.getElementById('songList')
+const nextButton = document.getElementById('next')
+const prevButton = document.getElementById('previous')
 
-DisplaySongs()
+let curIndex = 0
 
-async function DisplaySongs() {
-    const songs = await GetMusic()
-    console.log(songs)
+const pages = []
 
-    songs.forEach(song => {
-        container.appendChild(CreateElement(song))
-        
-    });
+GetMusic().then((data) => {
+    GenerateLists(data)
+    RenderSongs(pages[curIndex])
+})
+
+nextButton.addEventListener('click', () => {
+    curIndex++
+    RenderSongs(curIndex)
+}) 
+
+prevButton,addEventListener('click', () => {
+    if (curIndex > 0) {
+        curIndex--
+        RenderSongs(curIndex)
+    }
+})
+
+function RenderSongs(index) {
+    container.innerHTML = ''
+    container.appendChild(pages[curIndex])
 }
+
+function GeneratePage(songs) {
+
+    const slide = document.createElement('div')
+
+    for (let i = 0; i < songs.length; i++) {
+        const song = songs[i];
+        slide.innerHTML += CreateElement(song)
+        
+    }
+
+    return slide
+}
+
 
 function CreateElement(songData) {
 
-    const data = `<div class="bg-secondary rounded p-3">
+    const data = `<div style="z-index: 2000" class="mt-1 align-self-center ml-2 mr-2 bg-secondary rounded p-3">
     <h3>${songData.name}</h3>
     <p class="m-0">Autho: <span>${songData.artistName}</span></p>
     <p class="mt-0">Album: <span>${songData.albumName}</span></p>
@@ -26,8 +56,15 @@ function CreateElement(songData) {
     </audio>
   </div>`
 
-  const element = document.createElement('div')
-  element.innerHTML = data
 
-  return element
+
+  return data
+}
+
+function GenerateLists(songs) {
+    for (let i = 0; i < songs.length; i+=4) {
+
+        pages.push(GeneratePage(songs.slice(i, i+4)))
+
+    }
 }
